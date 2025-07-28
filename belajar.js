5f109b36a9f2597fd408d34716ba11d9e23acf6d
Bayyx. <bayurolenum08@gmail.com>
2025-07-28 09:52:40 +0700

    Add files via upload

---
+++ belajar.js
@@ -0,0 +1,23 @@
+// Menggunakan var
+var nama = "Bayu";
+console.log(nama); // Output: Bayu
+
+var nama = "Setiawan"; // Boleh diredeklarasi
+console.log(nama); // Output: Setiawan
+
+// Menggunakan let
+let usia = 16;
+console.log(usia); // Output: 16
+
+usia = 17; // Boleh diubah
+console.log(usia); // Output: 17
+
+// let usia = 18; // ❌ Error: tidak bisa diredeklarasi dalam scope yang sama
+
+// Menggunakan const
+const sekolah = "SMK Negeri 1";
+console.log(sekolah); // Output: SMK Negeri 1
+
+// sekolah = "SMK Negeri 2"; // ❌ Error: const tidak bisa diubah nilainya
+
+// const sekolah = "SMK Lain"; // ❌ Error: tidak bisa diredeklarasi
